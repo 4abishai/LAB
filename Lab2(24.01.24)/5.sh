20d6c4aeb28ba7749791c9fc51a372a3194b963d
4abishai <146672653+4abishai@users.noreply.github.com>
2024-01-31 00:50:33 +0530

    Update 5.sh

--- Lab2(24.01.24)/5.sh
+++ Lab2(24.01.24)/5.sh
@@ -6,8 +6,9 @@ month=$(echo "$dateOut" | awk '{print $3}')
 year=$(echo "$dateOut" | awk '{print $4}')
 hour=$(echo "$dateOut" | awk -F'[: ]' '{print $5}')
 minute=$(echo "$dateOut" | awk -F'[ :]' '{print $6}')   # [ :] can also be written as [: ]
+seconds=$(echo "$dateOut" | awk -F'[: ]' '{print $7}')
 ampm=$(echo "$dateOut" | awk '{print $6}')
 
 
 # date/month/year/hour.min
-echo "$date/$month/$year/$hour:$minute:$hour $ampm"
+echo "$date/$month/$year/$hour:$minute:$seconds $ampm"
