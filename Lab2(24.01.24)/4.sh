a303b9cb77f614bbfc3897742121130319496bba
4abishai <146672653+4abishai@users.noreply.github.com>
2024-01-31 00:20:56 +0530

    Update 4.sh

--- Lab2(24.01.24)/4.sh
+++ Lab2(24.01.24)/4.sh
@@ -7,7 +7,6 @@ echo -e "\n"
 echo "$(ps -ux)"
 echo -e "\n"
 
-# echo "USER       PID  %CPU %MEM    VSZ   RSS TTY      STAT START   TIME COMMAND"
-# echo "USER       PID  PPID  %CPU %MEM    VSZ   RSS TTY      STAT START   TIME COMMAND"
-# ps -ef | awk '$2 == 2 {print}'
+echo "UID          PID    PPID  C STIME TTY          TIME CMD"
+ps -ef | awk '$2 == 2 {print}'
 
