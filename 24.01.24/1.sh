f05e1e4e4a569f5c4ba878e6e690606fecb883d0
4abishai <146672653+4abishai@users.noreply.github.com>
2024-02-05 02:34:59 +0530

    Update 1.sh

--- 24.01.24/1.sh
+++ 24.01.24/1.sh
@@ -4,7 +4,7 @@
 
 read -p "Enter year: " year
 
-if [[ $((year % 4)) -eq 0 && $((year % 100)) -ne 0 ]] || [[ $((year % 400)) -eq 0 ]]; then
+if [ $((year % 4)) -eq 0 ] && [ $((year % 100)) -ne 0 ] || [ $((year % 400)) -eq 0 ]; then
     echo "$year is a leap year"
 else
     echo "$year is not a leap year"
