da08816d2b461506fda6a0fd33696d3a4b5c341b
4abishai <146672653+4abishai@users.noreply.github.com>
2024-02-21 12:26:06 +0530

    added 3.sh

--- 7.02.24/3.sh
+++ 7.02.24/3.sh
@@ -1,11 +1,32 @@
 #!/bin/bash
 
-read -p "Enter file names: " -a files
+MAX_SIZE=1000  # 1000 bytes limit
 
-for ((i=0; i < ${#files[@]}; i++))
-do 
-    size=$(ls -l ${files[i]} | awk '{print $5}')
-    filename=$(echo "${files[i]}" | awk -F[.txt] '{print $1}')
-    
-    mkdir "$filename"_"$size"
+dir_count=1
+
+dir_size=0
+
+mkdir "x_$dir_count"
+
+# Loop through all files in the specified directory
+for file in *; do
+    if [ -f "$file" ]; then
+
+        file_size=$(ls -l "$file" | awk '{print $5}')
+
+        # Check if adding the file to the current directory exceeds the maximum size
+        if (( dir_size + file_size > MAX_SIZE )); then
+            # If the maximum size is exceeded, increment the directory counter and create a new directory
+            ((dir_count++))
+            mkdir "x_$dir_count"
+            # Reset directory size counter
+            dir_size=0
+        fi
+
+        # Move the file to the appropriate directory
+        mv "$file" "x_$dir_count/"
+        
+        # Update the directory size
+        dir_size=$((dir_size + file_size))
+    fi
 done
